6adcc2ee4a6c395693747937faf95798262d5537
susu <sud0.why@gmail.com>
2020-07-11 14:41:55 +0800

    Update Wrapper.py

--- Wrapper.py
+++ Wrapper.py
@@ -4,8 +4,10 @@ import torch
 from main import load_data_setting
 from main import load_model_decode
 
-input = sys.argv[1]
-input = input.decode("utf-8")
+input_ori = sys.argv[1]
+print(type(input_ori))
+input = input_ori.decode("utf-8")
+print(type(input))
 f = open("./data/input", "w")
 for each_word in list(input):
     f.write(each_word.encode("utf-8") + " O\n")
@@ -19,23 +21,54 @@ data.write_decoded_results("./data/output", decode_results, 'raw')
 
 f = open("./data/output", "r")
 lines = f.readlines()
-new_lines = []
-for line in lines:
-    if len(line) >= 2:
-        if line.decode("utf-8")[2] != "O":
-            new_lines.append(line[:-1])
-new_lines.append("   ")
+
+
+lines = [line for line in lines if len(line)>1]
+all = []
 result = ""
-flag = None
-for index in range(len(new_lines)):
-    if flag != new_lines[index][-3:]:
-        if flag:
-            result = result + flag + " "
-        result = result + new_lines[index].decode("utf-8")[:1].encode("utf-8")
-        flag = new_lines[index][-3:]
+index = 0
+print(len(lines))
+while index != len(lines):
+    if lines[index].decode("utf-8")[2] == u"B":
+        tag = lines[index].decode("utf-8")[4:7]
+        # print(tag)
+        for j in range(index + 1, len(lines)):
+            if lines[j].decode("utf-8")[2] == u"I":
+                pass
+            elif lines[j].decode("utf-8")[2] == u"O" or lines[j].decode("utf-8")[2] == u"B":
+                index = j + 1
+                break
+            else:
+                # print(str([index, j]))
+                print(input_ori.decode("utf-8")[index:j+1].encode("utf-8"))
+                # print(input.decode("utf-8")[index:j+1].encode("utf-8"))
+                # print(input.decode("utf-8")[index:j+1].encode("utf-8"))
+                all.append((tag, input_ori.decode("utf-8")[index:j+1].encode("utf-8")))
+                result = result + input_ori.decode("utf-8")[index:j+1].encode("utf-8") + tag.encode("utf-8") + " "
+                index = j + 1
+                break
     else:
-        result = result + new_lines[index].decode("utf-8")[:1].encode("utf-8")
-result = result + "\n"
-open("./data/output.format", "w").write(result)
+        index = index + 1
+print(all)
+
+
+# new_lines = []
+# for line in lines:
+#     if len(line) >= 2:
+#         if line.decode("utf-8")[2] != "O":
+#             new_lines.append(line[:-1])
+# new_lines.append("   ")
+# result = ""
+# flag = None
+# for index in range(len(new_lines)):
+#     if flag != new_lines[index][-3:]:
+#         if flag:
+#             result = result + flag + " "
+#         result = result + new_lines[index].decode("utf-8")[:1].encode("utf-8")
+#         flag = new_lines[index][-3:]
+#     else:
+#         result = result + new_lines[index].decode("utf-8")[:1].encode("utf-8")
+# result = result + "\n"
+open("./data/output.format", "w").write(result+"\n")
 print(result)
 print("done")
